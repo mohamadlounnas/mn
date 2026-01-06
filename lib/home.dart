6614047c09cc442723746109c4dca2053f128ba6
mohamadlounnas <mohamadlounnas@gmail.com>
2026-01-06 12:40:20 +0100

    feat: implement HomePage layout with Todo list and add floating action button

--- lib/home.dart
+++ lib/home.dart
@@ -3,11 +3,29 @@
 
 import 'package:flutter/material.dart';
 
+void _onpressed() {
+  print('Pressed');
+}
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo List'),
+      ),
+      body: ListView(
+        children: [
+          ListTile(
+            title: Text('Buy milk'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onpressed,
+        child: Text('Add'),
+      ),
+    );
   }
 }
